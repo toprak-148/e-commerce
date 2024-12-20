8c8872cc6cd4c5c11f5888150b376313047014f7
Toprak Dogan <toprakdgn148@gmail.com>
2024-12-21 00:32:33 +0300

    The db was updated and entities were created for the db tables in the backend

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/td005/ecommerce/entity/Address.java
@@ -0,0 +1,34 @@
+package com.td005.ecommerce.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "address")
+@Setter
+@Getter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "street")
+    private String street;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "state")
+    private String state;
+    @Column(name = "country")
+    private String country;
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private Order order;
+
+
+}
