2258fdd6953bea31e0e1166449b6430849d44998
Toprak Dogan <toprakdgn148@gmail.com>
2024-12-21 01:38:22 +0300

    controller bug fixed

--- 02-backend/spring-boot-ecommerce/src/main/java/com/td005/ecommerce/controller/CheckoutController.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/td005/ecommerce/controller/CheckoutController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
 @CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("api/checkout")
