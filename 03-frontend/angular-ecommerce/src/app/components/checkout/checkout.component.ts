e66321d79b357e06e0f0c5bca798e84128ce273a
Toprak Dogan <toprakdgn148@gmail.com>
2024-12-21 02:43:53 +0300

    Checkout form - Save The Order - Purchase

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Country } from 'src/app/common/country';
+import { Order } from 'src/app/common/order';
+import { OrderItem } from 'src/app/common/order-item';
+import { Purchase } from 'src/app/common/purchase';
 import { State } from 'src/app/common/state';
 import { CartService } from 'src/app/services/cart.service';
+import { CheckoutService } from 'src/app/services/checkout.service';
 import { Td005ShopFormService } from 'src/app/services/td005-shop-form.service';
 import { TD005Validators } from 'src/app/validators/td005-validators';
 
@@ -30,7 +35,9 @@ export class CheckoutComponent implements OnInit {
   //depedency injection
   constructor(private formBuilder:FormBuilder,
               private td005FormService:Td005ShopFormService,
-              private cartService:CartService
+              private cartService:CartService,
+              private checkoutService:CheckoutService,
+              private router:Router
   ) { }
 
   ngOnInit(): void {
@@ -122,13 +129,55 @@ export class CheckoutComponent implements OnInit {
     if(this.checkoutFormGroup.invalid)
     {
       this.checkoutFormGroup.markAllAsTouched();
+      return;
     }
 
-    console.log(this.checkoutFormGroup.get('customer').value);
+    //set up order
+    let order = new Order();
+    order.totalPrice = this.totalPrice;
+    order.totalQuantity = this.totalQuintity;
 
-    console.log("the shipping address country   is " + this.checkoutFormGroup.get('shippingAddress').value.country.name);
 
-    console.log("the shipping address state   is " + this.checkoutFormGroup.get('shippingAddress').value.state.name);
+    // get cart items
+    const cartItems = this.cartService.cardItems;
+
+    // create OrderItems from cartItems
+
+    //! let orderItems : OrderItem[] = [];
+    //! for(let i = 0 ; i < cartItems.length;i++){
+    //!   orderItems[i] = new OrderItem(cartItems[i]);
+
+    let orderItems:OrderItem[] = cartItems.map(tempCartItem => new OrderItem(tempCartItem));
+
+
+    // set up purchase
+    let purchase = new Purchase();
+
+    // populate purchase - customer
+    purchase.customer = this.checkoutFormGroup.controls['customer'].value;
+
+
+
+
+
+    // populate purchase - shipping address
+    purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress'].value;
+    const shippingState:State = JSON.parse(JSON.stringify(purchase.shippingAddress.state));
+    const shippingCountry:Country = JSON.parse(JSON.stringify(purchase.shippingAddress.country));
+    purchase.shippingAddress.state = shippingState.name;
+    purchase.shippingAddress.country = shippingCountry.name;
+
+    // populate purchase - billing address
+    purchase.billingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
+    const billingState:State = JSON.parse(JSON.stringify(purchase.billingAddress.state));
+    const billingCountry:Country = JSON.parse(JSON.stringify(purchase.billingAddress.country));
+    purchase.billingAddress.state = billingState.name;
+    purchase.billingAddress.country = billingCountry.name;
+
+    // populate purchase - order and orderItems
+
+    // call REST API via the CheckoutService
+
 
 
   }
